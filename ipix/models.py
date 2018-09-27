569c8e4640dedc9853cd2da94e2fda0eab2ca747
Evohmike <evohmik@gmail.com>
2018-09-27 10:44:47 +0300

    created class Location in models

--- ipix/models.py
+++ ipix/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    photo_location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()
+    
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.photo_location
+        
+
+class Category(models.Model):
+    photo_category = models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.photo_category
+
+
