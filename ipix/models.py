a70e9be6c2b7d6d02d4a416e66229355fc7ef6c0
Evohmike <evohmik@gmail.com>
2018-09-27 10:51:10 +0300

    created class category

--- ipix/models.py
+++ ipix/models.py
@@ -23,4 +23,25 @@ class Location(models.Model):
         return self.photo_location
 
 
+class Category(models.Model):
+    photo_category = models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.photo_category
+
 
