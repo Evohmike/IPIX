3cd004fb72665abe46b69a6f610e0e3bc6d6b2b5
Evohmike <evohmik@gmail.com>
2018-09-27 10:43:33 +0300

    This set the static file and added  a datababse

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ipix',
+        'USER': 'evohmike',
+    'PASSWORD':'1234',
     }
 }
+
 
 
 # Password validation
@@ -108,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -121,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
