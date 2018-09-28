d2a64fdb6309e5b3c69ed0c5bac28c6fefea991f
Evohmike <evohmik@gmail.com>
2018-09-29 00:26:33 +0300

    created search url

--- ipix/urls.py
+++ ipix/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.ipix,name='Home'),
-    # url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    url(r'^search/$', views.search, name='search_images'),
+
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
