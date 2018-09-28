03a9d79aba2bac29a6e5087285c35564aa08545e
Evohmike <evohmik@gmail.com>
2018-09-29 00:28:03 +0300

    created location filter function

--- ipix/views.py
+++ ipix/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
-
+from .models import Image,Location,Category
 # Create your views here.
 
 def ipix(request):
     title='Home'
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request, 'index.html',{'title':title,'images':images,'locations':locations})
 
-    return render(request, 'ipix.html')
+
+def location_filter(request, location):
+    locations = Location.objects.all()
+    images = Image.filter_by_location(location)
+    title = f'{location} Photos'
+    return render(request, 'location.html', {'title':title, 'images':images, 'locations':locations})
+
+
+
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        images_found = Image.search_image
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'images':images_found})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{'message':message})
+
