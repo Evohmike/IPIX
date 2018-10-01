37ee6a458dbe3ebb395db1ca6fa8ddcd27594fc9
Evohmike <evohmik@gmail.com>
2018-10-01 10:51:37 +0300

    modified views

--- ipix/views.py
+++ ipix/views.py
@@ -7,26 +7,33 @@ def ipix(request):
     title='Home'
     images = Image.objects.all()
     locations = Location.objects.all()
-    return render(request, 'index.html',{'title':title,'images':images,'locations':locations})
+    return render(request, 'ipix.html',{'title':title,'images':images,'locations':locations})
 
 
-def location_filter(request, location):
-    locations = Location.objects.all()
-    images = Image.filter_by_location(location)
-    title = f'{location} Photos'
-    return render(request, 'location.html', {'title':title, 'images':images, 'locations':locations})
+# def location_filter(request, location):
+#     locations = Location.objects.all()
+#     images = Image.filter_by_location(location)
+#     title = f'{location} Photos'
+#     return render(request, 'location.html', {'title':title, 'images':images, 'locations':locations})
+
+def filter_by_location(request,location_id):
+   """
+   Function that filters images by location
+   """
+   images = Image.filter_by_location(id= location_id)
+   return render (request, 'location.html', {"images":images})
 
 
-
-
-def search(request):
+def search_category(request):
     if 'category' in request.GET and request.GET['category']:
         search_term = request.GET.get('category')
-        images_found = Image.search_image
+        images_found = Image.search_image(search_term)
         message = f'{search_term}'
 
         return render(request, 'search.html',{'message':message, 'images':images_found})
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{'message':message})
+
+
 
