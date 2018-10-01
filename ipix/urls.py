94ac51e9721641e448dee6d0d6c6e2720f51c97c
Evohmike <evohmik@gmail.com>
2018-10-01 10:51:14 +0300

    created location url

--- ipix/urls.py
+++ ipix/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.ipix,name='Home'),
-    url(r'^search/$', views.search, name='search_images'),
+    url(r'^search/', views.search_category, name='search_category'),
+    url(r'^location/(\d+)', views.filter_by_location, name='location_filter'),
 
 
 ]
